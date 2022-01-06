c1f22aaa775187f46924677d101be79ab6bd2319
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2022-01-06 18:45:02 -0500

    Basic anki cards

--- parse_romaji.py
+++ parse_romaji.py
@@ -1,9 +1,11 @@
 """
-Creates a dictionary of all the unique romaji words in the transcript
-and their english counterparts
+Creates Romaji -> English Anki cards
 """
 import re
 import subprocess
+import genanki
+import random
+# print(random.randrange(1 << 30, 1 << 31))
 
 def removeLongVowels(str):
   new_str = str
@@ -14,87 +16,70 @@ def removeLongVowels(str):
   new_str = new_str.replace("ū", "uu")
   return new_str
 
-romaji_words = []
-with open('episode 1/romaji.txt','r') as file:
-  for line in file:
-    for word in line.split():
-    # just punctuation
-      if re.search('[a-zA-Z]', word) == None:
-        continue
-      romaji_words.append(word)
+def createRomajiToEnglishDict():
+  # Create a set of the unique romaji words
+  romaji_words = []
+  with open('episode 1/romaji.txt','r') as file:
+    for line in file:
+      for word in line.split():
+      # just punctuation
+        if re.search('[a-zA-Z]', word) == None:
+          continue
+        romaji_words.append(word)
+  unique_romaji_words = set(romaji_words)
+  print(unique_romaji_words, len(unique_romaji_words))
 
-# romaji_transcript = 'episode 1/romaji2.txt'
+  # Get the definition of each word using myougiden
+  # and store it in a romaji_to_english dictionary
+  romaji_to_english_dict = {}
+  unknown_words = []
+  count = 0
+  for word in unique_romaji_words:
+    print("word", count)
+    # Remove long vowels fromy word to work with myougiden dictionary search
+    result = subprocess.run(['myougiden', '-r', removeLongVowels(word)], capture_output=True)
+    if result.returncode == 0:
+      definition = result.stdout.decode("utf-8")
+      romaji_to_english_dict[word] = definition
+    else:
+      print("Unknown word", word)
+      unknown_words.append(word)
+    count += 1
 
-# lines = None
-# with open(romaji_transcript) as f:
-#   lines = f.read().splitlines()
+  print("Created Episode Dictionary of size " + str(len(romaji_to_english_dict)))
+  print("\n")
+  print(str(len(unknown_words)) + " words were unknown:", unknown_words)
+  return romaji_to_english_dict
 
-# # print(len(lines))/
-
-# # dialogue is every 4 lines starting at line 2
-# i = 2
-# while i < len(lines):
-#   print(lines[i], i)
-#   # dialogue.append(lines[i])
-
-#   i += 4
-
-unique_romaji_words = set(romaji_words)
-print(unique_romaji_words, len(unique_romaji_words))
-
-# Get the definition of each word using myougiden
-# and store it in a romaji_to_english dictionary
 romaji_to_english_dict = {}
-unknown_words = []
-count = 0
-for word in unique_romaji_words:
-  print("word", count)
-  # Remove long vowels fromy word to work with myougiden dictionary search
-  result = subprocess.run(['myougiden', '-r', removeLongVowels(word)], capture_output=True)
-  if result.returncode == 0:
-    definition = result.stdout.decode("utf-8")
-    romaji_to_english_dict[word] = definition
-  else:
-    print("Unknown word", word)
-    unknown_words.append(word)
+for i in range(10):
+  romaji_to_english_dict["Romaji " + str(i)] = "English " + str(i)
 
-  count += 1
-  # if count == 10:
-  #   break
+my_model = genanki.Model(
+  1407607172,
+  'Simple Model',
+  fields=[
+    {'name': 'Question'},
+    {'name': 'Answer'},
+  ],
+  templates=[
+    {
+      'name': 'Romaji To English',
+      'qfmt': '{{Question}}',
+      'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
+    },
+  ])
 
-print("Created Episode Dictionary of size " + str(len(romaji_to_english_dict)))
-print(romaji_to_english_dict)
-print("\n")
-print(str(len(unknown_words)) + " words wer unknown:", unknown_words)
+my_deck = genanki.Deck(
+  1170750336,
+  'Naruto Episode 1 Vocab')
 
-"""
-The current problem is that there are too many unknown words.
-Why are there so many unknown words? (347)
-1) Some of the romaji are written with a long vowel.
-- When you pass the word written this way to myougiden it can't find the word,
-but when you remove it it is usually able to find it. So I could write a function
-to substite long vowels with their regular characters.
-- But even that's still a small percentage of the unknown words (maybe 30).
-- Actually that brought it down a decent bit. The number of unknown words is now
-286 so that brought it down by 61 words.
+for romaji, english in romaji_to_english_dict.items():
+  my_deck.add_note(
+    genanki.Note(
+      model=my_model,
+      fields=[romaji, english]
+    )
+  )
 
-2) Some of the words only come in after the ending song. I don't really need to know this part
-so I can try removing these. Doing this and pointing to the right Romaji file brought down
-the # unknown words to 260
-
-3) I think this is the biggest issue. The transcript file that I'm using has no spaces between
-individual words or if it does, the spacing is inconsistent. Thus when I copy and paste it into
-Romajidesu it tries finding the romaji for some combined words instead of the individual words
-and ends up generating something like "hairuga" instead of "hairu" and "ga". If can get a correctly
-spaced transcript file in the first place this should fix this. On animelon the subs appear to be
-spaced perfectly. I could either copy them one by one into my own transcript or figure out how to
-grab them from the source automatically. I did this and it's a little better but not nearly as
-much as I was hoping. Unknown words is now down to 216.
-
-4) Words are still being combined but this time it's the fault of Romajidesu. Even though I space
-out each word individually it's romaji translator is still combining some of them in the romaji
-translations (e.g. だけ です => dakedesu instead of dake desu). Is there a python tool to translate
-japanese to romaji? If so I could do it on a word by word level and probably get much more accurate translations.
-Or alternatively what if I created my dialogue file with each word only taking up one line. Would romajidesu
-be more accurate then? This was it lol down to 57 unknown words. This is manageable for an anlysis
-"""
+genanki.Package(my_deck).write_to_file('naruto_episode_1.apkg')
